83b2e5ac0ffeacaeb006fe7ae8e523c419d10561
Nicolas Lucchetta <lucchettan@gmail.com>
2020-01-15 12:02:06 +0100

    Creating the Xcode Project

---
+++ MargheritaApp/MargheritaApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  MargheritaApp WatchKit Extension
+//
+//  Created by Nicolas Lucchetta on 15/01/2020.
+//  Copyright © 2020 Nicolas Lucchetta. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
